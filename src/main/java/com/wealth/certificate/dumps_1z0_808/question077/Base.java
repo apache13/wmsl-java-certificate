2777cf7b8c59db92059a29745069d342d5a3b107
suphalak <30836315+suphalak@users.noreply.github.com>
2017-08-10 22:37:22 -0700

    com

--- src/main/java/com/wealth/certificate/dumps_1z0_808/question077/Base.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_808/question077/Base.java
@@ -2,7 +2,8 @@ package com.wealth.certificate.dumps_1z0_808.question077;
 
  class Base {
 	public static void main(String[] args) { 
-		System.out.println("Base " ); 
+		System.out.println("Base " + args[2]); 
 	} 
+	
 	
 }
