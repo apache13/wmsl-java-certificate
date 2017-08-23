ae7893905011603c48ba61342247609959a7dbec
wellyz35 <30769109+wellyz35@users.noreply.github.com>
2017-08-23 14:45:24 +0700

    Update Test.java

--- src/main/java/com/wealth/certificate/exam_1z0_808/question013/Test.java
+++ src/main/java/com/wealth/certificate/exam_1z0_808/question013/Test.java
@@ -15,15 +15,14 @@ class Animal {
 }
 
 class WildAnimal extends Animal {
-	String bound = "Long";
+	String bound;
 	
 	public WildAnimal(String bound) {
 		// n1
 	}
 	
 	public WildAnimal(String type,int maxSpeed, String bound) {
-		super(type,maxSpeed);
-		this.bound = bound;
+		//n2
 	}
 	
 	public void print() {
@@ -38,10 +37,24 @@ public class Test {
 		WildAnimal obj1 = new WildAnimal("Long");
 		obj1.print();
 		
-		WildAnimal obj2 = new WildAnimal("F....",80,"X....");
+		WildAnimal obj2 = new WildAnimal("Fedine",80,"Short");
 		obj2.print();
 		
 
 	}
 
 }
+/*
+Make Result
+Canine,60,Short
+Fedine,80,Long
+
+Answer. 
+at n1
+super();
+this.bound = bound;
+
+at n2
+super(type,maxSpeed);
+this.bound = bound;
+*/
