8c7dd215c8423fe0d1117a111460564451804cdc
apache13 <apache13@gmail.com>
2017-08-24 21:40:37 +0700

    add reservations.java

---
+++ src/test/java/Reservations.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reservations {
+	public static final int MAX = 128;	
+	public static final int SEQ= 1;
+	public static final int TOTAL= 14;
+	
+	public static void main(String[] args) {
+		List<Integer> examinationList = new ArrayList<>();
+		for(int i=1;i<=MAX;i++) {
+			examinationList.add(i);
+		}
+				
+		examinationList.stream().filter( (e) -> {return SEQ==(e%TOTAL)?true:false;} ).forEach((e)->System.out.println(e));
+		
+	}
+
+}
