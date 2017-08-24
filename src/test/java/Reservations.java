43025ada9b963dcd4cf612ed708eb2bac3ff71bb
apache13 <apache13@gmail.com>
2017-08-24 23:47:13 +0700

    update q043

--- src/test/java/Reservations.java
+++ src/test/java/Reservations.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Reservations {
-	public static final int MAX = 128;	
+	public static final int MAX_EXAM = 128;	
 	public static final int SEQ= 1;
 	public static final int TOTAL= 14;
 	
 	public static void main(String[] args) {
 		List<Integer> examinationList = new ArrayList<>();
-		for(int i=1;i<=MAX;i++) {
+		for(int i=1;i<=MAX_EXAM;i++) {
 			examinationList.add(i);
 		}
 				
