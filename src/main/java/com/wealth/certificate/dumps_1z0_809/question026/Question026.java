25febd0116fb54e982a6581e43a09bc997ac47a9
Sarunya-W <sarunya14w@gmail.com>
2017-08-25 09:33:27 +0700

    Fix Q26,40,54,82,124

--- src/main/java/com/wealth/certificate/dumps_1z0_809/question026/Question026.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809/question026/Question026.java
@@ -1,7 +1,5 @@
 package com.wealth.certificate.dumps_1z0_809.question026;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -23,9 +21,11 @@ class CallerThread implements Callable<String> {
 public class Question026 {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException { 
-		ExecutorService es = Executors.newFixedhreadPool(4); //line n1
-		Future f1 = es.submit (newCallerThread("Call"));
-		String str = f1.get().toString(); System.out.println(str);
+		ExecutorService es = Executors.newFixedThreadPool(4); //line n1
+		Future f1 = es.submit(new CallerThread("Call"));
+		String str = f1.get().toString();
+		System.out.println(str);
+//		es.shutdown(); //for shutdown the executor service since we don't need it anymore
 	}
 
 }
