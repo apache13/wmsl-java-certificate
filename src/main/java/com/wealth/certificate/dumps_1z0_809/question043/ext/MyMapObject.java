9123b423ab43c40a01222e7cbc7f5d6f3cc4a29c
apache13 <apache13@gmail.com>
2017-08-25 21:34:58 +0700

    update MyMapObject

--- src/main/java/com/wealth/certificate/dumps_1z0_809/question043/ext/MyMapObject.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809/question043/ext/MyMapObject.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class MyMapObject {
 
@@ -24,7 +25,7 @@ public class MyMapObject {
 		return lName;
 	}	
 			
-	public static void main(String[] args) {
+	public static <T> void main(String[] args) {
 		
 		List<MyMapObject> emp = Arrays.asList (new MyMapObject ("111", "333"), new MyMapObject ("222", "222"), new MyMapObject ("222", "111"));		
 		emp.stream().forEach(p -> System.out.println(p.getfName()+" "+p.getlName()) );
@@ -35,9 +36,14 @@ public class MyMapObject {
 			public String apply(MyMapObject t) {
 				return t.getfName();
 			}
-		};				
+		};		
+		// function ref
 		emp.stream().map(getFieldToMap).sorted(Comparator.reverseOrder()).forEach(p -> System.out.println(p) );
 		
+		// lambda
+		emp.stream().map( (e)->e.getfName() ).sorted(Comparator.reverseOrder()).forEach(p -> System.out.println(p) );
+				
+		// method ref
 		emp.stream().map(MyMapObject::getfName).sorted(Comparator.reverseOrder()).forEach(p -> System.out.println(p) );
 		
 		
