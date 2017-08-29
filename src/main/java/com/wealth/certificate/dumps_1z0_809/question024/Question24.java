9a7465e38bff91541da5b27149a448fea57ddf7a
natzuke06 <natzuke06@github.com>
2017-08-29 17:16:57 +0700

    Commit 809 (24)

--- src/main/java/com/wealth/certificate/dumps_1z0_809/question024/Question24.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809/question024/Question24.java
@@ -2,7 +2,9 @@ package com.wealth.certificate.dumps_1z0_809.question024;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 
 public class Question24 {
 
@@ -17,12 +19,22 @@ public class Question24 {
 
 		Duration duration = Duration.between(first.plusSeconds(100), second);
 		
-		System.out.println(duration);
+		Duration duration2 = Duration.between(LocalDateTime.now().plusHours(70), LocalDateTime.now());
 		
-		Duration duration2 = Duration.between(LocalDateTime.now().plusSeconds(70), LocalDateTime.now());
+		System.out.println("Duration :"+duration);
+		System.out.println("Duration :"+duration.toDays());
+		System.out.println("Duration 2 :"+duration2.toDays()); //PT-1M-10S
+		
 
-		System.out.println(duration2); //PT-1M-10S
+		//Period
 		
+		Period tenDays = Period.ofDays(10);
+        System.out.println(tenDays.getDays()); //10
+        
+        Period p2 = Period.between(LocalDate.now().plusDays(70), LocalDate.now());
+        System.out.println(p2);
 	}
 
 }
+
+//iso-8601
