1b5567ef42f73facf6fd6268a2daada0c43feac5
natzuke06 <natzuke06@github.com>
2017-08-31 21:33:51 +0700

    Update (24,38)

--- src/main/java/com/wealth/certificate/dumps_1z0_809/question024/Question24.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809/question024/Question24.java
@@ -5,6 +5,9 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Question24 {
 
@@ -15,11 +18,11 @@ public class Question24 {
 
 		// wait some time while something happens
 
-		Instant second = Instant.now();
+		Instant second = Instant.now(); //MachineTimeStamp
 
 		Duration duration = Duration.between(first.plusSeconds(100), second);
 		
-		Duration duration2 = Duration.between(LocalDateTime.now().plusHours(70), LocalDateTime.now());
+		Duration duration2 = Duration.between(LocalDateTime.now().plusSeconds(70), LocalDateTime.now());
 		
 		System.out.println("Duration :"+duration);
 		System.out.println("Duration :"+duration.toDays());
@@ -33,6 +36,16 @@ public class Question24 {
         
         Period p2 = Period.between(LocalDate.now().plusDays(70), LocalDate.now());
         System.out.println(p2);
+        
+        
+        //TimeZone
+        ZonedDateTime depart = ZonedDateTime.of(2015, 1, 15, 3, 0, 0, 0, ZoneId.of("UTC-5"));
+		ZonedDateTime arrive = ZonedDateTime.of(2015, 1, 15, 3, 0, 0, 0, ZoneId.of("UTC-5"));
+		long hrs = ChronoUnit.HOURS.between(depart, arrive); // line n1
+		
+		System.out.println(depart);
+		System.out.println(arrive);
+		System.out.println("Travel time is" + hrs + "hours");
 	}
 
 }
