adb7df877766dcefd4c27035a187ad6a97d203ed
cat <tobecat@gmail.com>
2017-09-02 20:30:40 +0700

    update 1z0-809 question 58,72,100

--- src/main/java/com/wealth/certificate/dumps_1z0_809/question100/Worker.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809/question100/Worker.java
@@ -12,26 +12,26 @@ public class Worker extends Thread {
 	public void run() {
 		try {
 			cb.await();
-			System.out.println("Worker�");
+			System.out.println("Worker�");
 		} catch (Exception ex) {
 		}
+	}
+	
+	public static void main(String[] args) {
+		Master master = new Master();
+		// line n2
+		CyclicBarrier cb = new CyclicBarrier(1, master);	//Choice C
+		Worker worker = new Worker(cb);
+		worker.start();
 	}
 }
 
 class Master implements Runnable { // line n1
 	public void run() {
-		System.out.println("Master�");
+		System.out.println("Master�");
 	}
 }
 
-class Test {
-	public static void main(String[] args) {
-		Master master = new Master();
-		// line n2
-		Worker worker = new Worker(cb);
-		worker.start();
-	}
-}
 
 /*
 You have been asked to ensure that the run methods of both the Worker and Master
@@ -43,6 +43,22 @@ A. At line n2, insert CyclicBarrier cb = new CyclicBarrier(2, master);
 B. Replace line n1 with class Master extends Thread {
 C. At line n2, insert CyclicBarrier cb = new CyclicBarrier(1, master);
 D. At line n2, insert CyclicBarrier cb = new CyclicBarrier(master);
-Answer: B
+
+Answer: C
+
+
+Explanation:
+CyclicBarrier: It provides a synchronization point (a barrier point) 
+where a thread may need to wait until all other threads also reach that point.
+นับจากจำนวนการ call .await()
+
+CyclicBarrier(int parties, Runnable barrierAction)
+Creates a CyclicBarrier with the specified number of threads waiting upon it, 
+and which will execute the given action when the barrier is reached.
+
+A. is wrong. There is only one worker start, so there is no way to reach barrier point of 2
+B. is wrong, It's still compilation failed.
+C. is correct
+D. is wrong. There is no constructor that accept only Runnable (Compilation failed)
 
 */
