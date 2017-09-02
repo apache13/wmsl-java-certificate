4e29cb4b2786538a0810ba280751a91628037c27
cat <tobecat@gmail.com>
2017-09-02 20:36:13 +0700

    update question 72

--- src/main/java/com/wealth/certificate/dumps_1z0_809/question072/TestAbstract.java
+++
@@ -1,6 +0,0 @@
-package com.wealth.certificate.dumps_1z0_809.question072;
-
-public abstract class TestAbstract {
-	public final int x = 0;
-	public static int y = 0;
-}
--- src/main/java/com/wealth/certificate/dumps_1z0_809/question072/TestInterface.java
+++
@@ -1,6 +0,0 @@
-package com.wealth.certificate.dumps_1z0_809.question072;
-
-public interface TestInterface {
-	public final int x = 0;
-	public static int y = 0;
-}
