fe68f8757565ac0b7b31a790c9688a8606df5e2a
cat <tobecat@gmail.com>
2017-09-05 23:15:54 +0700

    fix wrong commit 809 question 88

--- src/main/java/com/wealth/certificate/dumps_1z0_809/question088/Question088.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809/question088/Question088.java
@@ -17,23 +17,22 @@ public class Question088 {
 	public static void main(String[] args) {
 		UnaryOperator<Integer> uo1 = s -> s*2;//line n1 
 		List<Double> loanValues = Arrays.asList(1000.0, 2000.0); 
-		loanValues.stream().filter(lv -> lv >= 1500).map(lv -> uo1.apply(lv)).forEach(s -> System.out.print(s+""));
-//        System.out.println(uo1.apply(1));
-//		loanValues.stream().filter(lv -> lv >= 1500).map(lv -> uo1.apply(lv.intValue())).forEach(s -> System.out.print(s + " "));
-//		loanValues.stream().parallel().filter(lv -> lv >= 1500).map(lv -> new Double(1.0)).forEach(s -> System.out.print(s + " "));
+        System.out.println(uo1.apply(1));
+		loanValues.stream().filter(lv -> lv >= 1500).map(lv -> uo1.apply(lv.intValue())).forEach(s -> System.out.print(s + " "));
+		//loanValues.stream().parallel().filter(lv -> lv >= 1500).map(lv -> new Double(1.0)).forEach(s -> System.out.print(s + " "));
 		/* map receive function and return to stream , which UnaryOperator is Extend function but different of unaryOpertor is it's seem in / out the same parameter type
 		All of interface functional allow you to use lampda expression
 		map ::  <R> Stream<R> map(Function<? super T,? extends R> mapper)
 		flatMap :: <R> Stream<R> flatMap(Function<? super T,? extends Stream<? extends R>> mapper)
 		 */
 		
-//		List<String> nameCharList = employeeList.stream()
-//		           .map(emp-> emp.getName().split(""))
-//		           .flatMap(array->Arrays.stream(array))
-//		           .map(str -> str.toUpperCase()) 
-//		           .filter(str -> !(str.equals(" ")))
-//		           .collect(toList());
-//		   nameCharList.forEach(str -> System.out.print(str));
+		List<String> nameCharList = employeeList.stream()
+		           .map(emp-> emp.getName().split(""))
+		           .flatMap(array->Arrays.stream(array))
+		           .map(str -> str.toUpperCase()) 
+		           .filter(str -> !(str.equals(" ")))
+		           .collect(toList());
+		   nameCharList.forEach(str -> System.out.print(str));
 		   
 
 	}
