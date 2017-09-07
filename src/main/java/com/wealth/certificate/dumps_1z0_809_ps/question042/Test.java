4c908eaf7c7382ca660cde4e017a3648213ac473
Max Cpe <ratipong_kung@hotmail.com>
2017-09-07 11:27:50 +0700

    resolve conflict

--- src/main/java/com/wealth/certificate/dumps_1z0_809_ps/question042/Test.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809_ps/question042/Test.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.wealth.certificate.dumps_1z0_809_ps.question042;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -26,32 +25,3 @@ public class Test
 		/** Answered : B is incorrect, Correct is A print but not ordering because asynchronize thread. **/
 	}
 }
-=======
-package com.wealth.certificate.dumps_1z0_809_ps.question042;
-
-import java.util.concurrent.atomic.AtomicInteger;
-
-class MyThread implements Runnable {
-	private static AtomicInteger count = new AtomicInteger (0);
-	public void run () {
-		int x = count.incrementAndGet();
-		System.out.print (x+" ");
-	}
-}
-
-public class Test
-{
-	public static void main(String[] args)
-	{
-		Thread thread1 = new Thread(new MyThread());
-		Thread thread2 = new Thread(new MyThread());
-		Thread thread3 = new Thread(new MyThread());
-		Thread [] ta = {thread1, thread2, thread3};
-		for (int x= 0; x < 3; x++) {
-			ta[x].start();
-		}
-		
-		/** Answered : B is incorrect, Correct is A print but not ordering because asynchronize thread. **/
-	}
-}
->>>>>>> d66007e32a70cc5b69376e15612021d16704cd6d
