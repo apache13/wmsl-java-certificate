55f80fb8e623bacaf8e2b9d7104016e2e57dbfe8
panszy <pair.shaman@gmail.com>
2017-09-10 23:42:25 +0700

    edit note

--- src/main/java/com/wealth/certificate/dumps_1z0_809/question079/Moveable.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809/question079/Moveable.java
@@ -1,10 +1,29 @@
 package com.wealth.certificate.dumps_1z0_809.question079;
-
+@FunctionalInterface
 public interface Moveable<Integer> {
 	
 	public default void walk(Integer distance) {System.out.println("Walking");}
 
 	public void run(Integer distance);
+	//@Override
+	//public String equal(Object o1);
+}
+interface MyMoveable extends Moveable<Integer>{
+	public default void walk(Integer distance) {System.out.println("Walking 2");};
+}
+class MyClass implements MyMoveable{
+
+	@Override
+	public void run(java.lang.Integer distance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void walk(java.lang.Integer distance) {
+		// TODO Auto-generated method stub
+		MyMoveable.super.walk(10);	//if abst not call
+		this.walk(11);
+	}
 	
 }
-
