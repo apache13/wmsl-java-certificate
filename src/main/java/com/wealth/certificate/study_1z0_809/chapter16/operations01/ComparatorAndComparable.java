f6b7ec63c703dabc9f7a40ee80fa6c3205fc72d3
apache13 <apache13@gmail.com>
2017-09-13 22:37:13 +0700

    initial chapter16,17

---
+++ src/main/java/com/wealth/certificate/study_1z0_809/chapter16/operations01/ComparatorAndComparable.java
@@ -0,0 +1,10 @@
+package com.wealth.certificate.study_1z0_809.chapter16.operations01;
+
+public class ComparatorAndComparable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
