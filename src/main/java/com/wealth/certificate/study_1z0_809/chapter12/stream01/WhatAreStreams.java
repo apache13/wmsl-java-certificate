2b63d6dd9c9b8c2eb9ba455f1935b8080dd423d6
apache13 <apache13@gmail.com>
2017-09-13 23:39:19 +0700

    update c12

--- src/main/java/com/wealth/certificate/study_1z0_809/chapter12/stream01/WhatAreStreams.java
+++ src/main/java/com/wealth/certificate/study_1z0_809/chapter12/stream01/WhatAreStreams.java
@@ -1,120 +1,99 @@
 package com.wealth.certificate.study_1z0_809.chapter12.stream01;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-class Student {
-	double score;
-	String name;
-
-	public double getScore() {
-		return score;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setScore(double score) {
-		this.score = score;
-	}
-
-	public Student(String name, double score) {
-		this.name = name;
-		this.score = score;
-	}
-
-	@Override
-	public String toString() {
-		return name + " (" + score + ")";
-	}
-
-}
 
 public class WhatAreStreams {
 
 	public static void main(String[] args) {
 
-		List<Student> students = Arrays.asList(new Student("A", 95.0), new Student("B", 91.0), new Student("C", 99.0));
-
-		List<Student> studentsScore = new ArrayList<Student>();
-		for (Student s : students) {
-			if (s.getScore() > 90.0) {
-				studentsScore.add(s);
-			}
-		}
-		Collections.sort(studentsScore, new Comparator<Student>() {
-			public int compare(Student s1, Student s2) {
-				return Double.compare(s1.getScore(), s2.getScore());
-			}
-		});
-
-		for (Student student : studentsScore) {
-			System.out.println(student);
-		}
-
-		// Using streams
-		System.out.println("----- Using stream -----");
-		List<Student> studentsScore2 = students.stream().filter(s -> s.getScore() >= 90.0)
-				.sorted(Comparator.comparing(Student::getScore)).collect(Collectors.toList());
-
-		for (Student student : studentsScore2) {
-			System.out.println(student);
-		}
+		/*
+		 * What are streams?
+		 */
+		
 		
 		/*
-		What are streams?
-		
-		First, streams are NOT collections.		
-		A simple definition is that streams are WRAPPERS for collections and arrays, 
-		They wrap an EXISTING collection (or another data source) to support operations expressed with LAMBDAS
-		*/
-		
-		// ## Streams work perfectly with lambdas.
-			Stream.of(1,2,3).filter(e-> e>2).forEach(System.out::println); //print 3
-
-		/* ## Streams don't store its elements.
-		* The elements are stored in a collection or generated on the fly. They are only carried from the source through a pipeline of operations.
-		*/
-			
-		/* ## Streams are immutable.
-		* Streams don't mutate their underlying source of elements. 
-		*/	
-			
-		/* ## Streams are not reusable.
-		 * Streams can be traversed only once.
-		 * A stream should be operated on (invoking an intermediate or terminal stream operation) only once.
-		 * A stream implementation may throw IllegalStateException if it detects that the stream is being reused.
+		 * First, streams are NOT collections. A simple definition is that streams are
+		 * WRAPPERS for collections and arrays, They wrap an EXISTING collection (or
+		 * another data source) to support operations expressed with LAMBDAS
 		 */
-			Stream<String> stream = Stream.of("A","A","I","O","O");
-			Stream<String> distict = stream.distinct();
-		//long count = stream.count();
-		
-		
-		/* ## Streams don't support indexed access to their elements.	
-		* streams are not collections or arrays. The most you can do is get their first element.
-		*/
-		
-		// Streams are easily parallelizable.	
-		
-			Integer[] st = Stream.of(1,3,5,7,9).parallel().filter(i -> i%2 == 1).toArray(Integer[]::new);
-			System.out.println(Arrays.toString(st));
-			
-		/* ## Stream operations are lazy when possible.
-		*An important feature of intermediate operations is that they don't process the elements until a terminal operation 
-		is invoked, in other words, they're lazy.
-		*/	
-			System.out.println(Stream.of(1,2,3).filter(e -> e>1).count()); // print 2 because 2 and 3 greater than 1
+		// 1. Streams work perfectly with lambdas.
+		Stream.of(1, 2, 3).filter(e -> e > 2).forEach(System.out::println); // print 3
 
+		
+		/*
+		 * 2. Streams don't store its elements. The elements are stored in a collection
+		 * or generated on the fly. They are only carried from the source through a
+		 * pipeline of operations.
+		 */
+
+		/*
+		 * 3. Streams are immutable. Streams don't mutate their underlying source of
+		 * elements.
+		 */
+
+		/*
+		 * 4. Streams are not reusable. Streams can be traversed only once. A stream
+		 * should be operated on (invoking an intermediate or terminal stream operation)
+		 * only once. A stream implementation may throw IllegalStateException if it
+		 * detects that the stream is being reused.
+		 */
+		Stream<String> stream = Stream.of("A", "A", "I", "O", "O");
+		Stream<String> distict = stream.distinct();
+		// long count = stream.count(); // java.lang.IllegalStateException: stream has already been operated upon or closed
+
+		/*
+		 * 5. Streams don't support indexed access to their elements. streams are not
+		 * collections or arrays. The most you can do is get their first element.
+		 */
+
+		
+		// 6. Streams are easily parallelizable.
+		IntStream s1 = IntStream.range(0,100_000_000);
+		IntStream s2 = IntStream.range(0,100_000_000);
+		
+		LocalTime start = LocalTime.now();
+		s1.sequential().filter( p-> p%2 == 0 ).forEach(e->{});		
+		LocalTime finish = LocalTime.now();
+		System.out.println("Sequential using time : "+Duration.between(start, finish).toMillis()+" ms");
+		
+		start = LocalTime.now();
+		s2.parallel().filter( p-> p%2 == 0 ).forEach(e->{});
+		finish = LocalTime.now();
+		System.out.println("Parallel using time : "+Duration.between(start, finish).toMillis()+" ms");
+		
+		
+		/*
+		 * 7. Stream operations are lazy when possible. An important feature of
+		 * intermediate operations is that they don't process the elements until a
+		 * terminal operation is invoked, in other words, they're lazy.
+		 */		
+		start = LocalTime.now();
+		Stream.of(1, 2, 3).filter(e -> {
+			try {
+				TimeUnit.MILLISECONDS.sleep(500);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+			return true;
+		});
+		finish = LocalTime.now();
+		System.out.println("Lazy using time : "+Duration.between(start, finish).toNanos()+ " ns");
+		
+		start = LocalTime.now();
+		Stream.of(1, 2, 3).filter(e -> {
+			try {
+				TimeUnit.MILLISECONDS.sleep(500);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+			return true;
+		}).count();
+		finish = LocalTime.now();
+		System.out.println("Lazy using time : "+Duration.between(start, finish).toNanos()+" ns");
 	}
 
 }
