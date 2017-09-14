db6808574000400b1acc52c5e4184ec85824fe91
Max Cpe <ratipong_kung@hotmail.com>
2017-09-14 23:51:14 +0700

    ปรับ Intermediate
    รอ merge

--- src/main/java/com/wealth/certificate/study_1z0_809/chapter12/stream03/IntermediateOperations.java
+++ src/main/java/com/wealth/certificate/study_1z0_809/chapter12/stream03/IntermediateOperations.java
@@ -1,5 +1,8 @@
 package com.wealth.certificate.study_1z0_809.chapter12.stream03;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.temporal.Temporal;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -11,8 +14,8 @@ public class IntermediateOperations {
 
 		/* 
 		 * Intermediate Operations แบ่งออกเป็น 2 ประเภท
-		 * 1. Stateless Operations เป็น state ที่ element เป็นอิสระต่อกัน ***
-		 * 2. Stateful Operations เป็น state ที่รวม element ก่อนหน้าเข้าด้วยกันเพื่อจะ processing element ใหม่
+		 * 1. Stateless Operations เป็น state ที่ไม่ต้องใช้ element ก่อนหน้าในการ processing element ใหม่ โดยแต่ละ element เป็นอิสระต่อกัน
+		 * 2. Stateful Operations เป็น state ที่ต้องใช้ element ก่อนหน้าในการ processing element ใหม่
 		 */
 		
 		/*
@@ -32,11 +35,6 @@ public class IntermediateOperations {
 		System.out.print("List<String>: ");
 		List<String> slDist = Arrays.asList("1", "1", "2", "3", "4");
 		slDist.stream().distinct().forEach(System.out::print);
-		System.out.println();
-		
-		System.out.print("Stream<String>: ");
-		Stream<String> ssDist = Stream.of("1", "1", "2", "3", "4");
-		ssDist.distinct().forEach(System.out::print);
 		System.out.println("\n");
 		
 		/*
@@ -48,11 +46,6 @@ public class IntermediateOperations {
 		System.out.print("List<String>: ");
 		List<String> slLimit = Arrays.asList("1", "1", "2", "3", "4");
 		slLimit.stream().limit(3).forEach(System.out::print);
-		System.out.println();
-		
-		System.out.print("Stream<String>: ");
-		Stream<String> ssLimit = Stream.of("1", "1", "2", "3", "4");
-		ssLimit.limit(3).forEach(System.out::print);
 		System.out.println("\n");
 		
 		/*
@@ -61,11 +54,6 @@ public class IntermediateOperations {
 		 * Description: Return Stream ค่าที่เหลืออยู่จากการข้ามไปจำนวน n ตัว โดยเริ่มนับตั้งแต่ตัวแรก
 		 */
 		System.out.println("# Example: Stream<T> skip(long n)");
-		System.out.print("List<String>: ");
-		List<String> slSkip = Arrays.asList("1","1","2","3","4");
-	    slSkip.stream().skip(2).forEach(System.out::print);
-	    System.out.println();
-	    
 	    System.out.print("Stream<String>: ");
 	    Stream<String> ssSkip = Stream.of("1", "1", "2", "3", "4");
 		ssSkip.skip(2).forEach(System.out::print);
@@ -76,12 +64,7 @@ public class IntermediateOperations {
 		 * Type: Stateful 
 		 * Description: Return Stream ที่ถูกจัดเรียงตาม Natural order
 		 */
-		System.out.println("# Example: Stream<T> sorted()");
-		System.out.print("List<String>: ");
-		List<String> slSort = Arrays.asList("3","5","1","4","3","2");
-	    slSort.stream().sorted().forEach(System.out::print);
-	    System.out.println();
-	    
+		System.out.println("# Example: Stream<T> sorted()");	    
 	    System.out.print("Stream<String>: ");
 	    Stream<String> ssSort = Stream.of("3","5","1","4","3","2");
 	    ssSort.sorted().forEach(System.out::print);
@@ -96,17 +79,12 @@ public class IntermediateOperations {
 		System.out.print("List<String>: ");
 		List<String> slSortComp = Arrays.asList("3","5","1","4","3","2");
 	    slSortComp.stream().sorted(Comparator.reverseOrder()).forEach(System.out::print);
-	    System.out.println();
-	    
-	    System.out.print("Stream<String>: ");
-	    Stream<String> ssSortComp = Stream.of("3","5","1","4","3","2");
-	    ssSortComp.sorted(Comparator.naturalOrder()).forEach(System.out::print);
 		System.out.println("\n");
 		
 		/*
 		 * Example: Stream<T> filter(Predicate<? super T> predicate)
 		 * Type: Stateless
-		 * Description: Return Stream ที่ Element ตรงกับ Predicate ที่ให้ (true)
+		 * Description: Return Stream ที่มี Element ผ่านเงื่อนไข
 		 */
 		System.out.println("# Stream<T> filter(Predicate<? super T> predicate)");
 		System.out.print("List<Integer>: ");
@@ -114,9 +92,8 @@ public class IntermediateOperations {
 		nlFilter.stream().filter(n -> n % 2 == 0).forEach(System.out::print);
 		System.out.println();
 		
-		System.out.print("Stream<Integer>: ");
-		Stream<Integer> nsFilter = Stream.of(1,2,3,4,5,6);
-		nsFilter.filter(n -> n % 3 == 0).forEach(System.out::print);
+		System.out.print("Stream: ");
+		Stream.of(1,2,3,4,5,6).filter(n -> n % 3 == 0).forEach(System.out::print);
 		System.out.println("\n");
 		
 		/*
@@ -127,12 +104,11 @@ public class IntermediateOperations {
 		System.out.println("# Example: <R> Stream<R> flatMap(Function<? super T,? extends Stream<? extends R>> mapper");
 		System.out.print("List<String>: ");
 		List<String> slFlatMap = Arrays.asList("1.0", "1", "2.0", "3", "4");
-		slFlatMap.stream().flatMap(n -> Stream.of(n)).forEach(System.out::print);
+		slFlatMap.stream().flatMap(n -> Stream.of("A", "b", "C", "d", "E")).forEach(System.out::print);
 	    System.out.println();
 	    
-	    System.out.print("Stream<String>: ");
-	    Stream<String> ssFlatMap = Stream.of("A", "b", "C", "d", "E");
-	    ssFlatMap.flatMap(n -> Stream.of(n)).forEach(System.out::print);
+	    System.out.print("Stream: ");
+	    Stream.of("A", "b", "C", "d", "E").flatMap(n -> Stream.of(n)).forEach(System.out::print);
 		System.out.println("\n");
 		
 		/*
@@ -146,9 +122,8 @@ public class IntermediateOperations {
 		nlMap.stream().map(n -> n * 2).forEach(System.out::print);
 		System.out.println();
 		
-		System.out.print("Stream<Integer>: ");
-		Stream<Integer> nsMap = Stream.of(1,2,3);
-		nsMap.map(n -> n * 3).forEach(System.out::print);
+		System.out.print("Stream: ");
+		Stream.of(1,2,3).map(n -> n * 3).forEach(System.out::print);
 		System.out.println("\n");
 		
 		/*
@@ -162,9 +137,8 @@ public class IntermediateOperations {
 		slPeek.stream().limit(3).peek(n -> System.out.print(n)).forEach(System.out::print);
 		System.out.println();
 		
-		System.out.print("Stream<String>: ");
-		Stream<String> ssPeek = Stream.of("1", "1", "2", "3", "4");
-		ssPeek.limit(3).peek(n -> System.out.print("| |")).forEach(System.out::print);
+		System.out.print("Stream: ");
+		Stream.of("1", "1", "2", "3", "4").limit(3).peek(n -> System.out.print("| |")).forEach(System.out::print);
 		System.out.println("\n");
 		
 		/*
@@ -172,18 +146,33 @@ public class IntermediateOperations {
 		 * Type: N/A
 		 * Description: 
 		 */
+		System.out.println("# Example: Stream<T> parallel()");
+		LocalTime start = LocalTime.now();
+		Stream.of(1, 1, 2, 3, 4).parallel().filter(n -> n % 3 == 0).forEach(e -> {});
+		LocalTime finish = LocalTime.now();
+		System.out.println("Parallel using time : "+Duration.between(start, finish).toMillis()+" ms");
+		System.out.println("\n");
 		
 		/*
 		 * Example: Stream<T> sequential()
 		 * Type: N/A
 		 * Description: 
 		 */
+		System.out.println("# Example: Stream<T> sequential()");
+		start = LocalTime.now();
+		Stream.of(1, 1, 2, 3, 4).sequential().filter(n -> n % 3 == 0).forEach(e -> {});
+		finish = LocalTime.now();
+		System.out.println("Parallel using time : "+Duration.between(start, finish).toMillis()+" ms");
+		System.out.println("\n");
 		
 		/*
 		 * Example: Stream<T> unordered()
 		 * Type: N/A
 		 * Description: 
 		 */
+		System.out.println("# Example: Stream<T> unordered()");
+		Stream.of(1, 1, 2, 3, 4).unordered().forEach(System.out::print);
+		System.out.println("\n");
 	}
 
 }
