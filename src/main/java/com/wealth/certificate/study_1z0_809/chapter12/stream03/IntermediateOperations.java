754c63c96462bd0705a54c608ba5e11fe8830dc3
Max Cpe <ratipong_kung@hotmail.com>
2017-09-15 01:53:14 +0700

    Intermediate เพิ่ม 3 ตัว

--- src/main/java/com/wealth/certificate/study_1z0_809/chapter12/stream03/IntermediateOperations.java
+++ src/main/java/com/wealth/certificate/study_1z0_809/chapter12/stream03/IntermediateOperations.java
@@ -3,6 +3,7 @@ package com.wealth.certificate.study_1z0_809.chapter12.stream03;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -26,12 +27,13 @@ public class IntermediateOperations {
 		
 		Stream<String> s = Stream.of("m", "k", "c", "t", "a").sorted().limit(3);
 		s.forEach(System.out::println);
+		System.out.println();
 		
 		
 		/* 1. Stream<T> parallel()   Type: N/A   Returns an equivalent stream that is parallel. */
 		System.out.println("1. Stream<T> parallel()   Type: N/A   Returns an equivalent stream that is parallel.");
 		LocalTime start = LocalTime.now();
-		IntStream.range(1, 21)
+		IntStream.rangeClosed(1, 20)
 				.parallel()
 				.forEach(e -> {
 					try {
@@ -47,7 +49,7 @@ public class IntermediateOperations {
 		/* 2. Stream<T> sequential()   Type: N/A   Returns an equivalent stream that is sequential. */
 		System.out.println("2. Stream<T> sequential()   Type: N/A   Returns an equivalent stream that is sequential.");
 		start = LocalTime.now();
-		IntStream.range(1, 21)
+		IntStream.rangeClosed(1, 20)
 				.sequential()
 				.forEach(e -> {
 					try {
@@ -63,29 +65,32 @@ public class IntermediateOperations {
 		/* 3. Stream<T> unordered()   Type: N/A   Returns an equivalent stream that is unordered. */
 		System.out.println("3. Stream<T> unordered()   Type: N/A   Returns an equivalent stream that is unordered.");
 		//IntStream.range(1, 21).unordered().peek(n -> System.out.print(n)).forEach(e -> System.out.println());
-		IntStream numberStream = IntStream.range(1, 21);
-		System.out.print("skip-skip-unordered-toList: ");
-		numberStream.parallel().filter(n -> n != 0)
+		List<Integer> numberStream = IntStream.rangeClosed(1, 20).boxed().collect(Collectors.toList());
+		System.out.println("skip-skip-unordered-toList: " +
+		numberStream.parallelStream()
+				.filter(n -> n != 0)
 				.skip(1)
 				.skip(1)
 				.unordered()
-				.forEach(System.out::print);
+				.collect(Collectors.toList()));
 		
-		numberStream = IntStream.range(1, 21);
-		System.out.println("skip-unordered-skip-toList: ");
-		numberStream.parallel().filter(x -> x > 0)
+		numberStream = IntStream.rangeClosed(1, 20).boxed().collect(Collectors.toList());
+		System.out.println("skip-unordered-skip-toList: " +
+		numberStream.parallelStream()
+				.filter(n -> n != 0)
 	            .skip(1)
 	            .unordered()
 	            .skip(1)
-	            .forEach(System.out::print);
+	            .collect(Collectors.toList()));
 		
-		numberStream = IntStream.range(1, 21);
-		System.out.println("unordered-skip-skip-toList: ");
-		numberStream.parallel().filter(x -> x > 0)
+		numberStream = IntStream.rangeClosed(1, 20).boxed().collect(Collectors.toList());
+		System.out.println("unordered-skip-skip-toList: " +
+		numberStream.parallelStream()
+				.filter(n -> n != 0)
 	            .unordered()
 	            .skip(1)
 	            .skip(1)
-	            .forEach(System.out::print);
+	            .collect(Collectors.toList()));
 		
 	}
 
