3437d527db03e71c9c41d0e9c304e37053a93526
Max Cpe <ratipong_kung@hotmail.com>
2017-09-17 23:52:19 +0700

    flatMap
    สร้างเนื้อหา ยังไม่ได้ implement

--- src/main/java/com/wealth/certificate/study_1z0_809/chapter17/stream03/FlatMap.java
+++ src/main/java/com/wealth/certificate/study_1z0_809/chapter17/stream03/FlatMap.java
@@ -1,9 +1,39 @@
 package com.wealth.certificate.study_1z0_809.chapter17.stream03;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class FlatMap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/* flatMap() is used to "flatten" (or combine) the elements of a stream into one (new) stream: */
+		
+		/*
+		 * <R> Stream<R> flatMap(Function<? super T, ? extends Stream<? extends R>> mapper)
+		 * 
+		 * DoubleStream flatMapToDouble(Function<? super T, ? extends DoubleStream> mapper)
+		 * 
+		 * IntStream flatMapToInt(Function<? super T, ? extends IntStream> mapper)
+		 * 
+		 * LongStream flatMapToLong(Function<? super T, ? extends LongStream> mapper)
+		 */
+		
+		/*
+		 * Stream<String[]>		-> flatMap ->	Stream<String>
+		 * Stream<Set<String>>	-> flatMap ->	Stream<String>
+		 * Stream<List<String>>	-> flatMap ->	Stream<String>
+		 * Stream<List<Object>>	-> flatMap ->	Stream<Object>
+		*/
+		
+		Stream<List<String>> flatMapStream = Stream.of(Arrays.asList("a","b","c"),Arrays.asList("x","y","z"));			
+		long flatMapListSize = flatMapStream
+				.peek(p -> System.out.println("before " + p))
+				.flatMap(n -> n.stream() )
+				.peek(p -> System.out.println("after "+p))
+				.count();
+		System.out.println("flatMapListSize : " + flatMapListSize + "\n");
 
 	}
 
