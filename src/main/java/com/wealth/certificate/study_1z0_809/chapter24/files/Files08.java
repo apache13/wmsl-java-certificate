8a27b908745f8d3e76dba44f3bbe944030fac9b5
cat <tobecat@gmail.com>
2017-09-19 22:39:09 +0700

    update chapter 24

--- src/main/java/com/wealth/certificate/study_1z0_809/chapter24/files/Files08.java
+++ src/main/java/com/wealth/certificate/study_1z0_809/chapter24/files/Files08.java
@@ -4,29 +4,30 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.DosFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.util.concurrent.TimeUnit;
 
+//FileTime
 public class Files08 {
+	
 	public static void main(String[] args) {
 		try {
-			Path in = Paths.get(Files01.getCurrentPath() + "\\temp\\dir1\\in.txt");
-			Path out = Paths.get(Files01.getCurrentPath() + "\\temp\\dir2\\out.txt");
-
-			Path result = Files.copy(in, out, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
-
-			BasicFileAttributes basicIn = Files.readAttributes(in, BasicFileAttributes.class);
-
-			BasicFileAttributes basicOut = Files.readAttributes(out, BasicFileAttributes.class);
-
-			DosFileAttributes dosIn = Files.readAttributes(in, DosFileAttributes.class);
-
-			DosFileAttributes dosOut = Files.readAttributes(out, DosFileAttributes.class);
-
-			System.out.println(result);
+		Path path = Paths.get(Files01.getCurrentPath() + "\\temp\\dir1\\file.txt");
+		FileTime fileTime = Files.getLastModifiedTime(path);
+		System.out.println("getLastModifiedTime : " + fileTime);
+		
+		Files.setLastModifiedTime(path, FileTime.from(fileTime.toInstant()));
+		System.out.println("from(Instant instant) : " + Files.getLastModifiedTime(path));
+		
+		Files.setLastModifiedTime(path, FileTime.from(5, TimeUnit.DAYS));
+		System.out.println("from(long value, TimeUnit unit) : " + Files.getLastModifiedTime(path));
+		
+		Files.setLastModifiedTime(path, FileTime.fromMillis(fileTime.toMillis() + 1000));
+		System.out.println("fromMillis(long value) : " + Files.getLastModifiedTime(path));
+		
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
 }
