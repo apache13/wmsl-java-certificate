dfedd021fb40b4267a130c0239a1bcbdd6b11623
Sarunya-W <sarunya14w@gmail.com>
2017-09-23 03:40:14 +0700

    jdbc completely

--- src/main/java/com/wealth/certificate/study_1z0_809/chapter29/DBInit.java
+++ src/main/java/com/wealth/certificate/study_1z0_809/chapter29/DBInit.java
@@ -13,13 +13,7 @@ public class DBInit {
 
 		try (Connection conn = DriverManager.getConnection(URL)) {
 			try (Statement stt = conn.createStatement()) {
-				// create and insert employee
-//				stt.execute("CREATE TABLE contact(ID INT PRIMARY KEY, firstName varchar(30),\n" + 
-//						"lastName varchar(30), email varchar(30), phoneNo varchar(13)");
-//				stt.execute("INSERT INTO contact values (default, 'Michael', 'Taylor', 'michael@abc.com',\n" + 
-//						"'+919876543210')");
-//				stt.execute("INSERT INTO contact values (default, 'William', 'Becker', 'william@abc.com',\n" + 
-//						"'+449876543210')");
+				
 				stt.execute("CREATE TABLE EMPLOYEE(ID INT PRIMARY KEY ,NAME VARCHAR(30), PHONENO VARCHAR(30)) ");
 				stt.execute("INSERT INTO EMPLOYEE (ID,NAME,PHONENO) VALUES (111,'Michael','+919876543210')");
 				stt.execute("INSERT INTO EMPLOYEE (ID,NAME,PHONENO) VALUES (112,'William','+449876543210')");
--- src/main/java/com/wealth/certificate/study_1z0_809/chapter29/DBTest01.java
+++ src/main/java/com/wealth/certificate/study_1z0_809/chapter29/DBTest01.java
@@ -1,75 +1,31 @@
 package com.wealth.certificate.study_1z0_809.chapter29;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Properties;
 
 public class DBTest01 {
-	public static void main(String[] args) {
+	static Connection newConnection = null;
+	public static final String URL = "jdbc:derby:memory:chapter29;create=true";
 
-		String user = null;
-		String passw = null;
-		DBInit.init();
-		try (Connection con = DriverManager.getConnection(DBInit.URL, user, passw);
-				Statement stmt = con.createStatement();
-//				 Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_
-//				            SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-			) {
-			
-			boolean hasResults = stmt.execute("SELECT * FROM employee");
-			System.out.println("execute -> SELECT::"+hasResults);
-			if (hasResults) {
-				// To retrieve the object with the results
-				ResultSet rs = stmt.getResultSet();
-				System.out.println("ID \tNAME \tPHONENO");
-				while (rs.next()) {
-					System.out.println(rs.getInt("id") + "\t"
-					        + rs.getString("NAME") + "\t"
-					        + rs.getString("PHONENO"));
-				}
-			} else {
-				// To get the number of affected rows
-				int affectedRows = stmt.getUpdateCount();
-			}
-			System.out.println("--------After update---------");
-			
-			int result1 = stmt.executeUpdate("INSERT INTO EMPLOYEE (ID,NAME,PHONENO) VALUES (114,'George','+999000043210')"); // Returns 1
-			int result2 = stmt.executeUpdate("UPDATE EMPLOYEE SET NAME='Joe' " + "WHERE id = 111"); // Returns 1
-			int result3 = stmt.executeUpdate("DELETE FROM EMPLOYEE " + "WHERE id = 112"); // Returns 1
-			System.out.println("executeUpdate -> INSERT::"+result1+" UPDATE::"+result2+" DELETE::"+result3);
-			
-			System.out.println("executeQuery -> ResulSet");
-			System.out.println("(1)");
-			ResultSet rs = stmt.executeQuery("SELECT * FROM employee");
-			System.out.println("ID \tNAME \tPHONENO");
-			while (rs.next()) {
-				System.out.println(rs.getInt("id") + "\t"
-				        + rs.getString("NAME") + "\t"
-				        + rs.getString("PHONENO"));
-			}
-			
-			System.out.println("---------------------------------------");
-			System.out.println("(2)");
-			
-//			rs.beforeFirst();
-			rs = stmt.executeQuery("SELECT * FROM employee");
-			System.out.println("ID \tNAME \tPHONENO");
-			int numOfColumns = rs.getMetaData().getColumnCount();
-			while (rs.next()) {
-				for(int i = 1; i <= numOfColumns; i++) {
-					System.out.print(rs.getObject(i) + "\t");
-				}
-				System.out.println("");
-			}
-			
-//		    rs.absolute(1);
-//            rs.updateString("phoneNo", "+919976543210");
-             
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws SQLException {
+		
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test");
 
+		Connection conn2 = DriverManager.getConnection("jdbc:mysql://localhost/test", "db_admin", "db_pwd");
+
+		Properties props = new Properties();
+		props.put("user", "db_admin");
+		props.put("password", "db_pwd");
+		Connection conn3 = DriverManager.getConnection("jdbc:mysql://localhost/db", props);
+		
+		String url = "jdbc:mysql://localhost:3306/";
+		Driver driver = DriverManager.getDriver(url);
+		System.out.println(driver.getClass().getName()); // prints com.mysql.jdbc.Driver
+
+		Connection conn4 = driver.connect(url, props);
 	}
+
 }
