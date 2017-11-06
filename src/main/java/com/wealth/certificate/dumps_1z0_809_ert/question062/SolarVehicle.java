99d29dbad04c201ec6e36a072e04b16acd4f7f81
Perzyus <Perzyuss@gmail.com>
2017-11-07 00:42:45 +0700

    Questions: 10, 23, 36, 49, 62

---
+++ src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question062/SolarVehicle.java
@@ -0,0 +1,7 @@
+package com.wealth.certificate.dumps_1z0_809_ert.question062;
+
+public class SolarVehicle extends Vehicle {
+	public void ride() throws Exception { // line n2
+		super.ride();
+	}
+}
