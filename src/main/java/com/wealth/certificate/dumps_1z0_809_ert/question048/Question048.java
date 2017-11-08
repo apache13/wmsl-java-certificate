d4122e945b7fefc6f96d58a48516d950ffdcab4c
natzuke06 <natzuke06@github.com>
2017-11-08 14:31:08 +0700

    Commit [48,61]

--- src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question048/Question048.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question048/Question048.java
@@ -3,6 +3,7 @@ package com.wealth.certificate.dumps_1z0_809_ert.question048;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
 public class Question048 {
@@ -11,10 +12,18 @@ public class Question048 {
 		
 		// TODO Auto-generated method stub
 		final String str1 = "Java";
-		String strBuf = new String("Course");
+		StringBuffer strBuf = new StringBuffer("Course");
 		UnaryOperator<String> u = (str2) -> str1.concat(strBuf); //line n1
+		UnaryOperator<String> c = (str3) -> str3.toLowerCase(u); //line n2
+		System.out.println(u.apply(c.apply(strBuf)));
+		
+		/*
+		final String str1 = "Java";
+		String strBuf = new String("Course");
+		UnaryOperator<String> u = (str2) -> str1.concat(str2); //line n1
 		UnaryOperator<String> c = (str3) -> str3.toLowerCase(); //line n2
-		System.out.printIn(u.apply(c.apply(strBuf)));
+		System.out.println(u.apply(c.apply(strBuf)));
+		*/
 	}
 }
 
