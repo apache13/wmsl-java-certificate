a18acdab30764289aa151d5f4a808679e9515b8c
Perzyus <Perzyuss@gmail.com>
2017-11-10 18:01:58 +0700

    fixed: unclear logic

--- src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question062/Vehicle.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question062/Vehicle.java
@@ -1,7 +1,11 @@
 package com.wealth.certificate.dumps_1z0_809_ert.question062;
 
 public class Vehicle {
-	void ride() throws FuelNotAvailException { // line n1
+	//A) Replace line n1 with 
+//	public void ride() throws FuelNotAvailException {
+	//B) Replace line n1 with 
+	protected void ride() throws Exception { 
+//	void ride() throws FuelNotAvailException { // line n1
 		System.out.println("Happy Journey!");
 	}
 }
