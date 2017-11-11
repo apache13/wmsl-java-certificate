db1ee29fe6a7fc5bd01c38c59e525694ff8b6403
apache13 <apache13@gmail.com>
2017-11-11 17:43:10 +0700

    update answer

--- src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question044/App.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question044/App.java
@@ -8,6 +8,11 @@ public class App {
 		Console console = System.console();
 		char[] pass = console.readPassword("Enter password:"); // line 1
 		String password = new String(pass); // line 2
+		System.out.println(password);
 	}
-
+	// Run with command line
+	// java -cp [classpath] [mainclass]
+	// Example
+	// java -cp E:\Wealth\wmsl-java-certificate\target\classes com.wealth.certificate.dumps_1z0_809_ert.question044.App
+	
 }
