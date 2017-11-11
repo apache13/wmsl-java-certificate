48c6a709a5e237473526334a57a794f52ec59399
apache13 <apache13@gmail.com>
2017-11-11 21:15:55 +0700

    update answer

--- src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question069/Question069.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question069/Question069.java
@@ -4,21 +4,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Question069 {
 	public static void main(String[] args) {
 		Path source = Paths.get(getCurrentPath() +"/data/december/log.txt");
-		Path destination = Paths.get("/data");
+		Path destination = Paths.get(getCurrentPath() +"/data/log.txt");
 		try {
 			Files.copy(source, destination);
-			//Files.copy(Paths.get("/my/file.txt"), Paths.get("/my/output/directory/file.txt");
+			System.out.println("Success");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
 	private static String getCurrentPath() {
-		return System.getProperty("user.dir") + "/src/main/java/com/wealth/certificate/dumps_1z0_809/question053";
+		return System.getProperty("user.dir") + "/src/main/java/com/wealth/certificate/dumps_1z0_809_ert/question069";
 	}
 }
